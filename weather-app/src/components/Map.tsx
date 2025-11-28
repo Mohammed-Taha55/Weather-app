0dd1063928ebacea4384973ed65770f57a9f2096
Mohammed-Taha55 <mohammedtaha10683@gmail.com>
2025-11-28 16:33:14 +0530

    pointer-fix

--- weather-app/src/components/Map.tsx
+++ weather-app/src/components/Map.tsx
@@ -1,37 +1,47 @@
-
 import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 import type { Coords } from './types'
 import { useEffect } from 'react'
 import { MaptilerLayer } from "@maptiler/leaflet-maptilersdk"
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import shadowIcon from "leaflet/dist/images/marker-shadow.png";
+
+const DefaultIcon = L.icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: shadowIcon,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 const API_KEY = import.meta.env.VITE_API_KEY
-type Props = {
-  coords:Coords
-  onMapClick:(lat:number,lon:number)=>void
-  mapType:string
 
-}
+export default function Map({ coords, onMapClick, mapType }: Props) {
+  const { lat, lon } = coords
 
-export default function Map({coords, onMapClick,mapType}: Props) {
-  const {lat,lon} = coords
   return (
-   <MapContainer 
-   center={[lat, lon]} 
-   zoom={5} 
-   style={{width:"100%",height:"100%"}}
-   >
-    <MapClick onMapClick={onMapClick} coords={coords}/>
-   <MapTileLayer/>
-  <TileLayer 
-  opacity={0.7}
-  url={`https://tile.openweathermap.org/map/${mapType}/{z}/{x}/{y}.png?appid=${API_KEY}`}
-  />
-  <Marker position={[lat, lon]} />
+    <MapContainer 
+      center={[lat, lon]} 
+      zoom={5} 
+      style={{ width:"100%", height:"100%" }}
+    >
+      <MapClick onMapClick={onMapClick} coords={coords}/>
+      <MapTileLayer />
 
- 
-</MapContainer>
+      <TileLayer 
+        opacity={0.7}
+        url={`https://tile.openweathermap.org/map/${mapType}/{z}/{x}/{y}.png?appid=${API_KEY}`}
+      />
+
+      <Marker position={[lat, lon]} />
+    </MapContainer>
   )
 }
+
 
 function MapClick({
   onMapClick,
